8fb9421ada7689a3910d0f324d404876804da4fa
melvincwng <melvincwng@users.noreply.github.com>
2021-02-01 15:51:40 +0800

    Added delete button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,19 @@ import React from "react";
 import './TodoItem.css';
 
 // destructure the new property "setTodo"
-const TodoItem = ({ name, isDone, setTodo }) => (
+const TodoItem = ({ name, isDone, setTodo, deleteTodo }) => (
     <div className="todo-item">
-      {/*on clicking the circle, toggle the status of the todo item*/}
+        {/*on clicking the circle, toggle the status of the todo item*/}
       <span className="todo-item__completed" onClick={() => setTodo(!isDone)}>
         {isDone && <img alt="done" src={`${process.env.PUBLIC_URL}/tick.png`} />}
       </span>
       <span className="todo-item__name">{name}</span>
+  
+      <span onClick={() => deleteTodo()} className="todo-item__delete">
+        X
+      </span>
     </div>
   );
+  
 
 export default TodoItem;
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -2,33 +2,53 @@ import React from "react";
 import TodoItem from './TodoItem';
 import { v4 as uuidv4 } from "uuid"; // 1. import the UUID
 
-class Todolist extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [
-                { id: uuidv4(), name: "Buy Milk", isDone: false}, // 2. add uuid to the item
-                { id: uuidv4(), name: "Do push up", isDone: true}, // 3. add uuid to the item
-            ],
-        };
-    }
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: uuidv4(), // 2. add uuid to the item
+          name: "Buy Milk",
+          isDone: false,
+        },
+        {
+          id: uuidv4(), // 3.add uuid to the item
+          name: "Do push up",
+          isDone: true,
+        },
+      ],
+    };
+  }
 
-    displayTodos() {
-        return this.state.todos.map((todo) => {
-          // 4. add a method that can change the status of isDone
-          const setTodo = (isDone) => {
-            const currentTodo = this.state.todos.filter(
-              (todoToFilter) => todoToFilter.id === todo.id
-            )[0];
-            currentTodo.isDone = isDone;
-            this.setState({ todos: [...this.state.todos] });
-          };
-          // 5. pass in the method as a prop to Todo component
-          return (
-            <TodoItem name={todo.name} isDone={todo.isDone} setTodo={setTodo} />
-          );
-        });
-      }
+  displayTodos() {
+    return this.state.todos.map((todo) => {
+      // 4. add a method that can change the status of isDone
+      const setTodo = (isDone) => {
+        const currentTodo = this.state.todos.filter(
+          (todoToFilter) => todoToFilter.id === todo.id
+        )[0];
+        currentTodo.isDone = isDone;
+        this.setState({ todos: [...this.state.todos] });
+      };
+
+      // add a delete function
+      const deleteTodo = () => {
+        const todosWithoutItem = this.state.todos.filter(
+          todoToFilter => todoToFilter.id !== todo.id,
+        );
+        this.setState({ todos: [...todosWithoutItem] });
+      };
+
+      // 5. pass in the method as a prop to Todo component
+      return (
+        <TodoItem name={todo.name} 
+        isDone={todo.isDone} 
+        setTodo={setTodo} 
+        deleteTodo={deleteTodo}/>
+      );
+    });
+  }
 
   render() {
     return (
@@ -40,4 +60,4 @@ class Todolist extends React.Component {
   }
 }
 
-export default Todolist;
+export default TodoList;
