fbd8a361280c53d5fcf527a650c3d05d208038af
melvincwng <melvinng93@outlook.com>
2022-03-25 14:31:28 +0800

    Add functionality to prevent user from creating a list if there is no new name for it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 import { v4 as uuidv4 } from "uuid";
-import Todolist from './components/Todolist';
-import Todolist2 from './components/Todolist2'
+import Todolist from "./components/Todolist";
+import Todolist2 from "./components/Todolist2";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Button, Col } from "react-bootstrap";
-
 
 function App() {
   const [newListName, setNewListName] = useState("");
@@ -17,6 +16,11 @@ function App() {
   ]);
 
   const addNewList = (e) => {
+    if (newListName === "") {
+      alert("Please add a name for the new list!");
+      return;
+    }
+
     e.preventDefault();
     setTodoLists([
       ...todoLists,
@@ -37,23 +41,25 @@ function App() {
       </Row>
       <Row>
         <form>
-          <input type="text" value={newListName} 
-          onChange={(e) => setNewListName(e.target.value)}
-          placeholder = 'name for new list'
-          aria-label="filter-text-2"
-          >
-          </input>
-          <button onClick={(e) => addNewList(e)}>Add new list</button>
+          <input
+            type="text"
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
+            placeholder="name for new list"
+            aria-label="filter-text-2"
+          ></input>
+          <button type="button" onClick={(e) => addNewList(e)}>
+            Add new list
+          </button>
         </form>
       </Row>
       <Row fluid className="justify-content-left">
-      {todoLists.map((todoList) => (
-        <Todolist key={todoList.id} title={todoList.title} />
-      ))}
+        {todoLists.map((todoList) => (
+          <Todolist key={todoList.id} title={todoList.title} />
+        ))}
       </Row>
     </Container>
   );
-} 
+}
 
 export default App;
-
